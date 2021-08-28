2f314e4d0a2c81e1c66015430be6e6ce57ce9338
Go-Gunji <go.eliteforce@gmail.com>
2021-08-28 12:41:39 +0900

    タイマー停止時にサウンド追加

--- app/src/main/java/com/example/kitchentimer/MainActivity.java
+++ app/src/main/java/com/example/kitchentimer/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.kitchentimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.Image;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -14,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     TextView mTimerText;
     ImageView mStartButton;
     ImageView mStopButton;
+    SoundPool mSoundPool;
+    int mSoundResId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         mStartButton = (ImageView) findViewById(R.id.timerStart);
         mStopButton = (ImageView) findViewById(R.id.timerStop);
 
-        CountDownTimer timer = new CountDownTimer(3*60*1000, 100) {
+        CountDownTimer timer = new CountDownTimer(5*1000, 100) {
             // 指定した間隔で実行したい処理
             @Override
             public void onTick(long millisUntilFinished) {
@@ -38,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 mTimerText.setText("0:00");
+                mSoundPool.play(mSoundResId, 1.0f, 1.0f, 0, 0, 1.0f);
             }
         };
 
@@ -52,7 +59,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 timer.cancel();
+                mSoundPool.stop(mSoundResId);
             }
         });
+    }
+
+    // 画面が表示されたときに呼ばれるメソッド
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mSoundPool = new SoundPool.Builder()
+                    .setMaxStreams(1)
+                    .setAudioAttributes(new AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_ALARM)
+                        .build())
+                    .build();
+        } else {
+            mSoundPool = new SoundPool(1, AudioManager.STREAM_ALARM, 0);
+        }
+        mSoundResId = mSoundPool.load(this, R.raw.bellsound, 1);
+    }
+
+    // 画面を閉じたときに呼ばれるメソッド
+    protected void onPause() {
+        super.onPause();
     }
 }
